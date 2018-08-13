8553589ac4716571e25e1d2e36491886b218f42d
Thor <thordoor89@hotmail.com>
2018-08-13 21:24:17 +0200

    Added create(still to be tested) and read_one

--- objects/product.php
+++ objects/product.php
@@ -29,4 +29,60 @@ class Product{
     
         return $stmt;
     }
+    function readOne(){
+ 
+        // query to read single record
+        $query = "SELECT
+                    *
+                FROM
+                    " . $this->table_name . "
+                WHERE
+                    id = ?
+                LIMIT
+                    0,1";
+     
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+     
+        // bind id of product to be updated
+        $stmt->bindParam(1, $this->id);
+     
+        // execute query
+        $stmt->execute();
+     
+        // get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+     
+        // set values to object properties
+        $this->description = $row['description'];
+        $this->type = $row['type'];
+    }
+    // create product
+    function create(){
+ 
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    description=:description, type=:type";
+        
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+        
+        // sanitize
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->type=htmlspecialchars(strip_tags($this->type));
+        
+        // bind values
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":name", $this->type);
+        
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+    
+        return false;
+     
+    }
 }
