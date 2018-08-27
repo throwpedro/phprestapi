d814fa010f02fcd69311dd4a0243b5172376eb0e
Thor <thordoor89@hotmail.com>
2018-08-27 22:10:40 +0200

    Added delete search and update files as well as functions un product php. Now to make front end and might add pagination later.

--- objects/product.php
+++ objects/product.php
@@ -1,36 +1,40 @@
 <?php
-class Product{
- 
+class Product
+{
+
     // database connection and table name
     private $conn;
     private $table_name = "Products";
- 
+
     // object properties
     public $id;
     public $description;
     public $type;
- 
+
     // constructor with $db as database connection
-    public function __construct($db){
+    public function __construct($db)
+    {
         $this->conn = $db;
     }
 
     // read products
-    function read(){
-    
+    public function read()
+    {
+
         // select all query
-        $query = "SELECT * FROM Products";
-    
+        $query = "SELECT * FROM $this->table_name";
+
         // prepare query statement
         $stmt = $this->conn->prepare($query);
-    
+
         // execute query
         $stmt->execute();
-    
+
         return $stmt;
     }
-    function readOne(){
- 
+    public function readOne()
+    {
+
         // query to read single record
         $query = "SELECT
                     *
@@ -40,49 +44,135 @@ class Product{
                     id = ?
                 LIMIT
                     0,1";
-     
+
         // prepare query statement
-        $stmt = $this->conn->prepare( $query );
-     
+        $stmt = $this->conn->prepare($query);
+
         // bind id of product to be updated
         $stmt->bindParam(1, $this->id);
-     
+
         // execute query
         $stmt->execute();
-     
+
         // get retrieved row
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-     
+
         // set values to object properties
         $this->description = $row['description'];
         $this->type = $row['type'];
     }
     // create product
-    function create(){
- 
+    public function create()
+    {
+
         // query to insert record
         $query = "INSERT INTO
                     " . $this->table_name . "
                 SET
                     description=:description, type=:type";
-        
+
         // prepare query
         $stmt = $this->conn->prepare($query);
-        
+
         // sanitize
-        $this->description=htmlspecialchars(strip_tags($this->description));
-        $this->type=htmlspecialchars(strip_tags($this->type));
-        
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->type = htmlspecialchars(strip_tags($this->type));
+
         // bind values
         $stmt->bindParam(":description", $this->description);
-        $stmt->bindParam(":name", $this->type);
-        
+        $stmt->bindParam(":type", $this->type);
+
         // execute query
-        if($stmt->execute()){
+        if ($stmt->execute()) {
             return true;
         }
-    
+
         return false;
-     
+
+    }
+    // update the product
+    public function update()
+    {
+
+        // update query
+        $query = "UPDATE
+                " . $this->table_name . "
+            SET
+                type = :type,
+                description = :description,
+            WHERE
+                id = :id";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->name = htmlspecialchars(strip_tags($this->type));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind new values
+        $stmt->bindParam(':name', $this->type);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':id', $this->id);
+
+        // execute the query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+    // delete the product
+    public function delete()
+    {
+
+        // delete query
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind id of record to delete
+        $stmt->bindParam(1, $this->id);
+
+        // execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+
+    }
+    // search products
+    public function search($keywords)
+    {
+
+        // select all query
+        $query = "SELECT
+                description, type
+            FROM
+                " . $this->table_name . "
+            WHERE
+                description LIKE ? or type LIKE ?";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $keywords = htmlspecialchars(strip_tags($keywords));
+        $keywords = "%{$keywords}%";
+
+        // bind
+        $stmt->bindParam(1, $keywords);
+        $stmt->bindParam(2, $keywords);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
     }
 }
